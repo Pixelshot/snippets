5a356c76985351d82698149d72931472b169a936
pixelshot <pixelshot8@gmail.com>
2023-12-27 12:49:29 +0800

    Edit + loading + not-found

---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1>{`Sorry, we couldn't find that particular snippet`}</h1>
+    </div>
+  );
+}
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
+import Link from 'next/link';
 
 interface SnippetShowPageProps {
   params: {
@@ -16,5 +17,25 @@ export default async function showSnippetPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
-  return <div>{snippet.title}</div>;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl font-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          {/* <Link> */}
+          <button className="p-2 border rounded">Delete</button>
+          {/* </Link> */}
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
