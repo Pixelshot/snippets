b93e881011ae5728fcf43b84da0d6f5a0a44d589
pixelshot <pixelshot8@gmail.com>
2023-12-30 22:31:03 +0800

    Split create into both client and server side. server in actions, client on pre-existing file. Incorporated useFormState() Hook to communicate between both Client side and Server side. Especially useful for letting user know why snippet can't be created yet.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,48 @@
 // This is where all the server functions that's needed on the Client-side are defined.
 // It's much easier to put everything into one file and call the ones that are needed from here.
+// Whenever Next.js sees 'use server' at the top of a function, it'll treat that function as a server side function
 'use server';
 
-import { db } from "@/db";
+import { db } from "@/db"; // Since index.ts is the only file in db, it will automatically be loaded
 import { redirect } from 'next/navigation';
+
+// This async function is to deal with the input submitted from the user on our form
+// It's async because it'll be dealing with the database, therefore it may take a bit of time to respond
+// Remember: All of the information the user has given is stored in formData, just like in Remix.run
+// This function is being called below in app>snippets>new>page.tsx
+// fomrstate is the object that we're passing on from the server to client side in new>page.tsx. See #42
+export async function createSnippet(formState: { message: string }, formData: FormData) {
+	// Check the user's inputs and make sure they're valid
+	// .get('title') and .get('name) is based on the 'name' property inside of HTML form element below
+	const title = formData.get('title');
+	const code = formData.get('code');
+
+	if (typeof title !== 'string' || title.length < 3) {
+		return {
+			message: "Title must be longer than 3 characters",
+		}
+	}
+
+	if (typeof code !== 'string' || code.length < 10) {
+		return {
+			message: 'Code must be longer than 10 characters',
+		}
+	}
+
+	// Create a new record in the database
+	const snippet = await db.snippet.create({
+		data: {
+			// Because key and values are the same, we can short cut with just the key
+			title,
+			code,
+		},
+	});
+
+	// TODO: Redirect the user back to the root route(see if we can show a flash message indicating the snippet was successfully created)
+	// User Next's redirect
+	redirect('/');
+}
+
 
 // To be used inside of snippet-edit-form.tsx
 export async function editSnippet(id: number, code: string) {
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,38 +1,18 @@
-import { redirect } from 'next/navigation';
-import { db } from '@/db'; // the only file in db(index.ts) will automatically be loaded
+'use client';
+
+import { useFormState } from 'react-dom';
+import * as actions from '@/actions';
 
 export default function SnippetCreatePage() {
-  // This async function is to deal with the input submitted from the user on our form
-  // It's async because it'll be dealing with the database, therefore it may take a bit of time to respond
-  // Remember: All of the information the user has given is stored in formData, just like in Remix.run
-  // This function is being called below in <form action={createSnippet}>
-  async function createSnippet(formData: FormData) {
-    // This needs to be a server action
-    // Whenever Next.js sees 'use server' at the top of a function, it'll treat that function as a server side function
-    'use server';
-
-    // Check the user's inputs and make sure they're valid
-    // .get('title') and .get('name) is based on the 'name' property inside of HTML form element below
-    // Temporarily we're just going to tell TS that these variables are indeed strings: See #22
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-
-    // Create a new record in the database
-    const snippet = await db.snippet.create({
-      data: {
-        // Because key and values are the same, we can short cut with just the key
-        title,
-        code,
-      },
-    });
-
-    console.log(snippet);
-    // Redirect the user back to the root route(see if we can show a flash message indicating the snippet was successfully created)
-    // User Next's redirect
-    redirect('/');
-  }
+  // fomrState is the object that'll be bouncing off between client side and server side
+  // We use this object to send and receive data to/from client to server and vice versa
+  // action will update actions.createSnippet. This action is attached to form action
+  // message with empty string argument is the initial state. Since it's a server action, it will not reach the server during first rendering. This will only trigger one a submission has been done. The empty string acts as a placeholder for when the server does respond
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: '',
+  });
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -57,6 +37,11 @@ export default function SnippetCreatePage() {
           />
         </div>
 
+        {formState.message ? (
+          <div className="my-2 p-2 bg-red-300 border rounded border-red-500">
+            {formState.message}
+          </div>
+        ) : null}
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
         </button>
